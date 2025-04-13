4fcc0c671f08acd45c97a563230adaed1b3e66e0
18web.cn <147181566@qq.com>
2025-04-13 16:14:49 +0800

    2025-4-13

---
+++ skin/28573/js/about.js
@@ -0,0 +1,9 @@
+// 出场动画
+$(document).on('ready scroll', function(event) {
+	divMove('.about-desc');
+	divMove('.about-title');
+	divMove('.good-one');
+	divMove('.about-partner');
+	divMove('.about-client');
+	divMove('.about-idea');
+});
---
+++ skin/28573/js/biz.js
@@ -0,0 +1,6 @@
+// 出场动画
+$(document).on('ready scroll', function(event) {
+	divMove('.biz-show-one');
+	divMove('.bizstep-list', 1);
+	divMove('.bstep-one');
+});
