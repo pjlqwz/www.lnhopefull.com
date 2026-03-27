57d460d96ea30834fb5968693edc964cb134a064
root <root@OpenClaw.18web.cn>
2026-03-27 10:07:54 +0800

    feat: 全新官网上线 - 一人公司/一人部门解决方案

--- skin/28573/js/about.js
+++
@@ -1,9 +0,0 @@
-// 出场动画
-$(document).on('ready scroll', function(event) {
-	divMove('.about-desc');
-	divMove('.about-title');
-	divMove('.good-one');
-	divMove('.about-partner');
-	divMove('.about-client');
-	divMove('.about-idea');
-});
